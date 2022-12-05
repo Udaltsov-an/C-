eb445caa121aa34fb6253b0d808cb608d36a6e47
Aleksandr Udaltsov <aln.udaltsov@yandex.ru>
2022-12-05 22:50:30 +0300

    Выполнен второй практический модуль

---
+++ Homework_002/Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+string num = number.ToString();
+Console.WriteLine($"Вторая цифра числа {number} - это {num[1]}");
---
+++ Homework_002/Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число: ");
+
+int number = int.Parse(Console.ReadLine());
+if(number < 100) {
+    Console.WriteLine("Третьей цифры нет!");
+}
+else {
+    string num = number.ToString();
+    Console.WriteLine($"Третья цифра - {num[2]}");
+}
