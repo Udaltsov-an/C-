44ce9003be1c1e083d1561395edd5dd91d1613a3
Aleksandr Udaltsov <aln.udaltsov@yandex.ru>
2022-12-06 20:43:10 +0300

    Добавили семинар 3

---
+++ Seminar_003/Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Методы
+int FindQuarter(int x, int y) {
+    int result = 0;
+    if(x > 0 && y > 0) result = 1;
+    if(x < 0 && y > 0) result = 2;
+    if(x < 0 && y < 0) result = 3;
+    if(x > 0 && y < 0) result = 4;
+    return result;
+}
+int EnterData(string text) {
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+// Запрос координат числа
+int x = EnterData("Введите координаты x: ");
+int y = EnterData("Введите координаты y: ");
+// Анализ данных
+int number = FindQuarter(x,y);
+// Вывод информации`
+Console.WriteLine($"Ваши координаты находятся в {number} четверти");
